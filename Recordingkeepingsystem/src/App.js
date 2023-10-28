31ccea7523ce26531a9a4c517e47cedaed6a4568
Sibgha Bhatti <106465420+SibghaBhatti@users.noreply.github.com>
2023-10-28 09:34:47 -0700

    Add files via upload

---
+++ Recordingkeepingsystem/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Fields from "./Components/Fields";
+const App = () => {
+  return (
+    <div>
+      <h1 className="head">STUDENT RECORD KEEPING SYSTEM</h1>
+
+      <Fields />
+    </div>
+  );
+};
+
+export default App;
